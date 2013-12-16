6ee70b0989854959404b19aa9c2d94bb0c05f60b
Alexander Mitin <amitin@AMITIN.scnsoft.com>
2013-12-16 17:50:22 +0300

    [#6963]
    Rolling back faultly commited tested data.

--- sites/all/themes/opendataomega/template.php
+++ sites/all/themes/opendataomega/template.php
@@ -5,6 +5,3 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Open Data Omega theme.
  */
-function opendataomega_form_alter(&$form, &$form_state, $form_id){
-    $b = true;
-}
