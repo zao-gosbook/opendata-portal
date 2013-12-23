f74c870eb67a84f5e9815f99167f77b9378439d0
Alexandr Todorozhko <namo86@gmail.com>
2013-12-23 15:10:59 +0400

    Change string format and add Drupal.t wrapper for strings

--- profiles/dkan/libraries/recline/src/view.flot.js
+++ profiles/dkan/libraries/recline/src/view.flot.js
@@ -26,8 +26,8 @@ my.Flot = Backbone.View.extend({
   template: ' \
     <div class="recline-flot"> \
       <div class="panel graph" style="display: block;"> \
-        <div class="js-temp-notice alert alert-block"> \\n\
-          ' + Drupal.t('<p>There\'s no graph here yet because we don\'t know what fields you\'d like to see plotted.<br />Please tell us by <strong>using the menu on the right</strong> and a graph will automatically appear.</p>') + '\
+        <div class="js-temp-notice alert alert-block"> \
+          <p>' + Drupal.t('There\'s no graph here yet because we don\'t know what fields you\'d like to see plotted.<br />Please tell us by <strong>using the menu on the right</strong> and a graph will automatically appear.') + '</p> \
         </div> \
       </div> \
     </div> \
