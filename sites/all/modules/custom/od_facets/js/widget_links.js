4e61ef6ba1ec0c9973df84813bc1d28823dc737c
p1ratrulezzz <g@g.ru>
2014-01-24 17:31:29 +0400

    Issue #6654: Fixed issue when search field's placeholder became a real value and submitted to a server. Note: fixed in right way

--- sites/all/modules/custom/od_facets/js/widget_links.js
+++ sites/all/modules/custom/od_facets/js/widget_links.js
@@ -12,18 +12,6 @@
           li.removeClass('facetapi-active');
         }
       });
-
-      //Ugly warkaround against bug with getting placeholder value with val()
-      if ($.valHooks !== undefined && typeof $.valHooks == 'object') {
-        $.valHooks.text = jQuery.valHooks['text'] === undefined ? new Object() : $.valHooks.text;
-        $.valHooks.text.get =  function (a) {
-          return a.getAttribute('value');
-        }
-
-        $.valHooks.text.set = function (a, b) {
-          return a.setAttribute('value', b);
-        }
-      }
     }
   }
 }) (jQuery);
