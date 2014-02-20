83ed3511c7ea0b7d379e47c66bcde03206c19be4
Max <mkashuba@inbox.ru>
2014-02-20 11:02:28 +0400

    Updates panels_tabs module

--- sites/all/modules/contrib/panels_tabs/js/panels_tabs.js
+++ sites/all/modules/contrib/panels_tabs/js/panels_tabs.js
@@ -4,25 +4,13 @@
  * JS related to the tabs in the Panels tabs.
  */
 Drupal.behaviors.panelsTabs = {
-  attach: function (context, settings) {
+  attach: function (context) {
     var tabsID = Drupal.settings.panelsTabs.tabsID;
-    var activeCookieName = 'panelsTabs:activeTab:'+tabsID;
-    var tabConfig = settings.panelsTabs.tabsConfig[tabsID] || {};
-    var saveActiveTab = tabConfig.saveActiveTab || false;
-
-    function callbackSaveActiveTab(event, ui) {
-      if (saveActiveTab) {
-        $.cookie(activeCookieName, ui.index, {path: window.location.href, expires: 365});
-      }
-    }
 
     for (var key in Drupal.settings.panelsTabs.tabsID) {
       $('#' + tabsID[key] +':not(.tabs-processed)', context)
         .addClass('tabs-processed')
-        .tabs({
-          selected: saveActiveTab ? ($.cookie(activeCookieName) || 0) : 0,
-          select: callbackSaveActiveTab
-        });
+        .tabs();
     }
   }
 };
