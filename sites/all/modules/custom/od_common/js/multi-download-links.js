7d777df61d81739bcfb0a36498ae89459575d90b
Ilgiz's working pc <i.abdullin@gosbook.ru>
2014-03-25 18:14:20 +0400

    Issue #8099 (Обновления ядра, модулей, общие фиксы): * fixed collapse/expand script to support multi widgets

--- sites/all/modules/custom/od_common/js/multi-download-links.js
+++ sites/all/modules/custom/od_common/js/multi-download-links.js
@@ -2,27 +2,30 @@
   Drupal.behaviors.od_common_multi_download_links = {
     attach: function(context, settings) {
       var $box = $('.multi-download-link-wrapper', context);
-      var $dropdownLinks = $box.find('.dropdown-links');
       $box.find('.main-download-link-wrapper>a').bind('click', function(e) {
         e.preventDefault();
+
+        var $this = $(this);
+        var $wrapper = $this.parents('.multi-download-link-wrapper:eq(0)');
+        var $dropdownLinks = $wrapper.find('.dropdown-links');
         var isExpanded = $dropdownLinks.hasClass('state-expanded') ? true : false;
         if (isExpanded) {
-          collapse();
+          collapse($dropdownLinks);
         }
         else {
-          expand();
+          expand($dropdownLinks);
         }
       })
 
-      // Collapse by default
-      collapse();
-      function collapse() {
+      // Collapse all by default
+      collapse($box.find('.dropdown-links'));
+      function collapse($dropdownLinks) {
         $dropdownLinks.slideUp();
         $dropdownLinks.removeClass('state-expanded');
         $dropdownLinks.addClass('state-collapsed');
       }
 
-      function expand() {
+      function expand($dropdownLinks) {
         $dropdownLinks.removeClass('element-invisible');
         $dropdownLinks.slideDown();
         $dropdownLinks.removeClass('state-collapsed');
