9e4dca3495d05f7f9809a9b6b71ab6453d7ad438
Denis Kuznetsov <denis.kuznetsov.kalistos@gmail.com>
2014-07-08 11:47:56 +0400

    Issue #10591 (Updated "Opendata API" library)

--- sites/all/libraries/od_api/odApi.php
+++ sites/all/libraries/od_api/odApi.php
@@ -3,10 +3,16 @@
 // Include base class.
 include_once 'classes/OdApi.inc';
 
-// Include base abstract classes.
+// Include helper class.
 include_once 'classes/OdApiHelper.inc';
+
+// Include base abstract classes.
 include_once 'classes/OdApiParser.inc';
 include_once 'classes/OdApiObject.inc';
+
+// Include parsers.
+include_once 'classes/parsers/OdApiRdfaParser.inc';
+include_once 'classes/parsers/OdApiCsvParser.inc';
 
 // Include object abstract classes.
 include_once 'classes/objects/default/OdApiMetaList.inc';
@@ -16,14 +22,18 @@ include_once 'classes/objects/default/OdApiDataList.inc';
 include_once 'classes/objects/default/OdApiData.inc';
 include_once 'classes/objects/default/OdApiStructure.inc';
 
-// Include parsers.
-include_once 'classes/parsers/OdApiRdfaParser.inc';
-include_once 'classes/parsers/OdApiCsvParser.inc';
-
-// Include object classes.
+// Include object 2.3 version classes.
 include_once 'classes/objects/versions/2.3/OdApiMetaList.inc';
 include_once 'classes/objects/versions/2.3/OdApiMeta.inc';
 include_once 'classes/objects/versions/2.3/OdApiPublisher.inc';
 include_once 'classes/objects/versions/2.3/OdApiDataList.inc';
 include_once 'classes/objects/versions/2.3/OdApiData.inc';
 include_once 'classes/objects/versions/2.3/OdApiStructure.inc';
+
+// Include object 3.0 version classes.
+include_once 'classes/objects/versions/3.0/OdApiMetaList.inc';
+include_once 'classes/objects/versions/3.0/OdApiMeta.inc';
+include_once 'classes/objects/versions/3.0/OdApiPublisher.inc';
+include_once 'classes/objects/versions/3.0/OdApiDataList.inc';
+include_once 'classes/objects/versions/3.0/OdApiData.inc';
+include_once 'classes/objects/versions/3.0/OdApiStructure.inc';
